dad5867da5a20c42ef49d072738c44d7787e9a0e
vishwas04 <66423959+vishwas04@users.noreply.github.com>
2020-10-23 19:12:22 +0530

    Add files via upload

---
+++ local_tigers/js.js
@@ -0,0 +1,16 @@
+// var script = document.createElement("script");
+// script.src = "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
+// script.type = "text/javascript";
+// document.getElementsByTagName("head")[0].appendChild(script);
+
+var points=0;
+
+//$("button").click(function() {
+//    var fired_button = $(this).val();
+//    alert(fired_button);
+//    console.log(1);
+//});
+//function add(event)
+//{
+//    console.log(event.target.value);
+//}
